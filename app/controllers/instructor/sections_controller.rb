20095d5fcd17cd7f9b1439a75a6fe22317aefb27
dino217 <brob217@gmail.com>
2016-01-25 14:09:24 -0800

    Added validations checking that a user is logged in and is authorized to view the instructor view of a course and to create a new course section.

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,23 +1,28 @@
 class Instructor::SectionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course
+
   def new
-    @course = Course.where(id: params[:course_id]).first
-    if @course.blank?
-      render text: "Not Found", status: :not_found
-    end
     @section = Section.new
   end
 
   def create
-    @course = Course.find(params[:course_id])
-    @section = @course.sections.build(section_params)
-    if @section.save
-      redirect_to instructor_course_path(@course)
-    else
-      render text: "Could not be created"
-    end
+    @section = current_course.sections.build(section_params)
+    redirect_to instructor_course_path(current_course)
   end
 
   private
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render text: "Unauthorized", status: :unauthorized
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:course_id])
+  end
 
   def section_params
     params.require(:section).permit(:title)
