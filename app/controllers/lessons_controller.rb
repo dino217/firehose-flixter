099f69b18bb5b4344118763dda1c18fad237c277
dino217 <brob217@gmail.com>
2016-01-27 22:09:04 -0800

    Now requires user to be enrolled in course before viewing lessons.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,21 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :validate_user_is_enrolled
+
   def show
 
   end
 
   private
+
+  def validate_user_is_enrolled
+    course = current_lesson.section.course
+    enrolled = current_user.enrolled_in?(course)
+    unless enrolled
+      redirect_to course_path(course), alert: "You must enroll in that course first."
+    end
+
+  end
 
   helper_method :current_lesson
   def current_lesson
