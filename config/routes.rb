d3509cc0cf23eff97e48b92a6f82fecc3f722e97
dino217 <brob217@gmail.com>
2016-01-27 21:54:33 -0800

    Students now able to enroll in courses. Enrollment model created to facilitate this.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'static_pages#index'
 
-  resources :courses, only: [:index, :show]
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end
   resources :lessons, only: [:show]
 
   namespace :instructor do
